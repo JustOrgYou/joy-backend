a50a73e1c73f9a12638b4e498e84d7e8c83d8035
tsepanx <77626319+tsepanx@users.noreply.github.com>
2023-06-19 09:57:48 +0300

    `npl_service/`

---
+++ nlp-service/firebase/question_entry.py
@@ -0,0 +1,22 @@
+class QuestionEntry:
+    _key: str
+    question: str
+    answer: str
+
+    def __init__(self, key: str, question: str, answer: str):
+        self._key = key
+        self.question = question
+        self.answer = answer
+
+    def __str__(self):
+        return self.question
+
+    def __eq__(self, other):
+        return (self.key == other.key) and (self.question == other.question) and (self.answer == other.answer)
+
+    def to_dict(self):
+        return {"key": self._key, "question": self.question, "answer": self.answer}
+
+    @property
+    def key(self):
+        return self._key
---
+++ nlp-service/nlp_router.py
@@ -0,0 +1,109 @@
+import logging
+import os
+import uvicorn
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+from firebase.firestore_database import FirestoreDatabase
+from firebase.question_entry import QuestionEntry
+from nlp.similarity_providers import sus_sim_provider
+
+SIMILARITY_CONST = 0.7
+app = FastAPI()
+fd = FirestoreDatabase(app_name="nlp_router")
+
+USE_NLP_ROUTER = os.getenv('USE_NLP_ROUTER', 'True').lower() == 'true'
+
+
+class QuestionItem(BaseModel):
+    question: str
+    answer: str
+
+
+@app.get("/", status_code=404)
+def index():
+    return Response(status_code=404)
+
+
+@app.get("/questions")
+def get_questions():
+    cur_questions = fd.questions()
+
+    answer = [q.to_dict() for q in cur_questions]
+    return answer
+
+
+@app.get("/question")
+def get_question(question: str):
+    cur_questions = fd.questions()
+
+    answer = [q.to_dict() for q in cur_questions if q.question == question]
+    if answer:
+        return answer[0]
+    else:
+        return JSONResponse({"status": "Such question does not exist"}, status_code=404)
+
+
+@app.post("/new-question")
+def set_question(q_item: QuestionItem):
+    q_key = sus_sim_provider.encode_question(q_item.question)
+    q_entry = QuestionEntry(q_key, q_item.question, q_item.answer)
+
+    fd.set_question(q_entry)
+
+    return JSONResponse(q_entry.to_dict() | {"status": "created"}, status_code=201)
+
+
+@app.delete("/delete-question")
+def delete_question(question: str):
+    cur_questions = fd.questions()
+
+    found = [q for q in cur_questions if q.question == question]
+    if not found:
+        return JSONResponse({"status": "Such question does not exist"}, status_code=404)
+
+    q = fd.get_question(question)
+    fd.delete_question(question)
+
+    return JSONResponse(q.to_dict() | {"status": "deleted"}, status_code=200)
+
+
+@app.get("/similar")
+def similar_questions(question: str, index: float = SIMILARITY_CONST):
+    """
+    /similar [GET]
+    :param question : question text
+    :param index : similarity threshold
+    """
+
+    import time
+    cur = time.time()
+    cur_questions = fd.questions()
+
+    question_key = sus_sim_provider.encode_question(question)
+    top_similar = []
+    for q in cur_questions:
+        try:
+            cur_sim = sus_sim_provider.similarity(question_key, q.key)
+            if cur_sim > index:
+                top_similar.append({"question": q.question, "answer": q.answer, "index": cur_sim})
+        except NameError:
+            pass
+
+    cur2 = time.time()
+
+    logging.warning(f'Similarity run time: {cur2 - cur}')
+
+    return top_similar
+
+
+def run_router():
+    uvicorn.run(app=app, host="0.0.0.0", port=8080)
+
+
+if __name__ == "__main__":
+    if USE_NLP_ROUTER:
+        run_router()
+    else:
+        print('USE_NLP_ROUTER = False')
